f274d9b03cd79c79664e682957bd5fe0fc389c58
DESKTOP-QV1R0KT\Administrator <1248694620@qq.com>
2019-05-02 20:21:13 +0800

    2019.05.02 20:21

--- MatrixUploader/MatrixUploader.cs
+++ MatrixUploader/MatrixUploader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 
@@ -7,19 +8,25 @@ namespace MatrixUploader
     public class MatrixUploader : Blog.IToBlog
     {
         private String filePath;
-        public void ToBlog(string parameter)
+      
+        public void ToBlog(List<string> parameters)
         {
-            string url = parameter;
+            string url = parameters[0];
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
 
-            WebClient webClient = new WebClient();
-            webClient.Encoding = Encoding.UTF8;
+                //这里使用DownloadString方法，如果是不需要对文件的文本内容做处理，直接保存，那么可以直接使用功能DownloadFile(url,savepath)直接进行文件保存。
+                webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
 
-            //这里使用DownloadString方法，如果是不需要对文件的文本内容做处理，直接保存，那么可以直接使用功能DownloadFile(url,savepath)直接进行文件保存。
-            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+                filePath = AppDomain.CurrentDomain.BaseDirectory + @"Download\" + url.Substring(url.LastIndexOf("/") + 1);
 
-            filePath = AppDomain.CurrentDomain.BaseDirectory + @"Download\" + url.Substring(url.LastIndexOf("/") + 1);
-
-            webClient.DownloadFileAsync(new Uri(url), filePath);
+                webClient.DownloadFileAsync(new Uri(url), filePath);
+            }
+            catch (Exception e) {
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
         }
 
         private void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
